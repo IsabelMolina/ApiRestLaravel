2260a155c547377aa644eb3aaf488d29daa15b88
IsabelMolina <isabelcmolina12@gmail.com>
2023-02-20 15:03:56 -0500

    first commit

---
+++ app/Http/Controllers/PetController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pet;
+
+class PetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pets = Pet::all();      
+        return $pets;  
+    }   
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        Pet::create($request->all()); 
+
+        return response()->noContent(201);                      
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $pet = Pet::findOrFail($id);    
+        return $pet;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        $pet = Pet::findOrFail($id);  
+        $pet->name = $request->name;
+        $pet->description = $request->description;
+        $pet->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $pet = Pet::findOrFail($id);
+        $pet->delete();
+        
+        return response()->noContent(204);
+    }
+}
